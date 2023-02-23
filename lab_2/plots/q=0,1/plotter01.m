1e6006ffd2b36214ca608050483d9cf56306bde3
Elias Olsen Almenningen <57808408+eliasoa@users.noreply.github.com>
2023-02-23 14:12:57 +0100

    Add files via upload

---
+++ lab_2/plots/q=0,1/plotter01.m
@@ -0,0 +1,48 @@
+% close all
+clear
+load('q=0k1.mat')
+load ('t.mat')
+load ('x1.mat')
+load ('x2.mat')
+load ('x3.mat')
+load ('x4.mat')
+% load ('fra_andre/u_q=10.mat')
+time = data(1,:);
+travel = data(2,:);
+travel_rate = data(3,:);
+pitch = data(4,:);
+pitch_rate = data(5,:);
+elevation = data(6,:);
+elevation_rate = data(7,:);
+
+
+stop = 55*501;
+
+figure(1)
+
+subplot(511)
+% stairs(t,u),grid
+subplot(512)
+plot(time(1:stop),travel(1:stop),'r');
+hold on 
+plot(t,x1,'b'),grid
+ylabel('lambda')
+
+subplot(513)
+plot(time(1:stop),travel_rate(1:stop),'r');
+hold on 
+plot(t,x2,'b'),grid
+ylabel('r')
+
+subplot(514)
+plot(time(1:stop),pitch(1:stop),'r');
+hold on 
+plot(t,x3,'b'),grid
+ylabel('p')
+
+subplot(515)
+plot(time(1:stop),pitch_rate(1:stop),'r');
+hold on 
+plot(t,x4,'b'),grid
+xlabel('tid (s)'),ylabel('pdot')
+legend()
